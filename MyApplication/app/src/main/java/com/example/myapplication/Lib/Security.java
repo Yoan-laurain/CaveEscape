b06a3be86b3faba35375659f00d638cf7edcb2d7
Yoan Laurain <80961060+verozgames@users.noreply.github.com>
2022-02-21 20:54:05 +0100

    Security + rename myApplication

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Lib/Security.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.Lib;
+
+public class Security
+{
+
+    public static String RandomToken(Integer nbLetters)
+    {
+        String randString = "";
+        String charUniverse = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\\=";
+
+        for( int i = 0; i < nbLetters; i++) {
+            randString += charUniverse.charAt( (int) (Math.random() * ( charUniverse.length() - 1)));
+        }
+        return randString;
+    }
+}
