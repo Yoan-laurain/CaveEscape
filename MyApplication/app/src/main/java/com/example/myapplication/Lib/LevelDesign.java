b7a9f21cbb2916e73036f87c3a03ddf16da3582c
Yoan Laurain <80961060+verozgames@users.noreply.github.com>
2022-02-27 15:16:03 +0100

    Implementing display of the list of the levels and open level associated

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Lib/LevelDesign.java
@@ -0,0 +1,66 @@
+package com.example.myapplication.Lib;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.myapplication.LevelSelect.SelectActivity;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+
+public class LevelDesign extends ArrayAdapter
+{
+
+    // ---------------------------------------------
+    private ArrayList<String> title;
+    private int idLayout;
+    private Activity context;
+
+
+    // ---------------------------------------------
+
+    public LevelDesign(Context context, int idLayout, ArrayList<String> title)
+    {
+        super(context, idLayout,title);
+        this.title = title;
+        this.idLayout = idLayout;
+        this.context = (Activity) context;
+    }
+
+    // ---------------------------------------------
+
+    // ---------------------------------------------
+
+    /*
+        Called when used setAdapter method
+        Set items of row.xlm and return the object
+     */
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View row;
+        if(convertView == null)
+        {
+            LayoutInflater inflater = context.getLayoutInflater();
+            row = inflater.inflate(idLayout, null, true);
+        } else
+        {
+            row = convertView;
+        }
+
+        TextView textViewTitle = row.findViewById(R.id.TitreLevel);
+        textViewTitle.setText(title.get(position));
+
+        return  row;
+    }
+    // ---------------------------------------------
+
+}
