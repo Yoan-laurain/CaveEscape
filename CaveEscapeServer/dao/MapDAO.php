8b163a17d3e090a6c1a8c67fadebbd5243a21375
HugH-CRT <61247861+HugH-CRT@users.noreply.github.com>
2022-03-08 17:15:05 +0100

    fix gradle + api

--- CaveEscapeServer/dao/MapDAO.php
+++ CaveEscapeServer/dao/MapDAO.php
@@ -7,10 +7,11 @@ class MapDAO {
     public static function GetAllMap(){
         DBConnex::runFetchAll(
             "Trying to get all maps",
-            "Sucess retrieving all maps",
+            "Success retrieving all maps",
             "Failure retrieving all maps",
             "SELECT *
-            FROM map"
+            FROM map
+            where isTested = true"
         );
     }
 
@@ -19,7 +20,7 @@ class MapDAO {
     public static function GetMapByClient(){
         DBConnex::runFetchAll(
             "Trying to get all maps of a client",
-            "Sucess retrieving all maps of a client",
+            "Success retrieving all maps of a client",
             "Failure retrieving all maps of a client",
             "SELECT *
             FROM map
@@ -33,7 +34,7 @@ class MapDAO {
     public static function SaveMap(){
         DBConnex::runQuery(
             "Trying to insert a new map",
-            "Sucess to insert a new map",
+            "Success to insert a new map",
             "Failure to insert a new map",
             "INSERT INTO map (nom,nbRows,nbColumns,idClient) VALUES ( :nameMap, :nbRows, :nbColumns, :idClient );",
             array("nameMap","nbRows","nbColumns","idClient")
@@ -41,7 +42,7 @@ class MapDAO {
 
         DBConnex::runFetch(
             "Trying to get max id Map",
-            "Sucess retrieving max id Map",
+            "Success retrieving max id Map",
             "Failure retrieving max id Map",
             "SELECT max(idMap) as id
             FROM map"
@@ -54,7 +55,7 @@ class MapDAO {
     public static function UpdateMap(){
         DBConnex::runQuery(
             "Trying to update a map",
-            "Sucess to update a map",
+            "Success to update a map",
             "Failure to update a map",
             "UPDATE map SET nom = :nameMap, nbRows = :nbRows, nbColumns = :nbColumns, idClient = :idClient where idMap = :idMap;",
             array("nameMap","nbRows","nbColumns","idClient","idMap")
@@ -63,11 +64,23 @@ class MapDAO {
 
     // --------------------------------------------
 
+    public static function UpdateIsTested(){
+        DBConnex::runQuery(
+            "Trying to update isTested in a map",
+            "Sucess to update isTested in a map",
+            "Failure to update isTested in a map",
+            "UPDATE map SET isTested = :tested where idMap = :idMap;",
+            array("idMap", "tested")
+        );  
+    }
+
+    // --------------------------------------------
+
     public static function DeleteMap(){
         
         DBConnex::runQuery(
             "Trying to delete all map_lines of a map",
-            "Sucess to delete all map_lines of a map",
+            "Success to delete all map_lines of a map",
             "Failure to delete all map_lines of a map",
             "DELETE FROM map_ligne WHERE idMap = :idMap",
             array("idMap")
@@ -75,11 +88,13 @@ class MapDAO {
         
         DBConnex::runQuery(
             "Trying to delete a map",
-            "Sucess to delete a map",
+            "Success to delete a map",
             "Failure to delete a map",
             "DELETE FROM map WHERE idMap = :idMap",
             array("idMap")
         );
     }
+
+
     
 }
