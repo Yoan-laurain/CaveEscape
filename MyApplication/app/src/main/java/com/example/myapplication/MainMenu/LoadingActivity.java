08b3d1cc0b5a20ae018bd09dc935a10544583238
Yoan Laurain <80961060+verozgames@users.noreply.github.com>
2022-03-14 18:40:09 +0100

    change api connexion from local to api

--- MyApplication/app/src/main/java/com/example/myapplication/MainMenu/LoadingActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainMenu/LoadingActivity.java
@@ -44,9 +44,9 @@ public class LoadingActivity extends AppCompatActivity
 
     // ---------------------------------- Variables ----------------------------------------------//
     HashMap params = new HashMap<>();
-    //public final static String CONNEXION_API = "http://51.254.96.53:8383/api.php";
+    public final static String CONNEXION_API = "http://51.254.96.53:8383/api.php";
     public final static String API_KEY = "43d24893a0ca4dfdacbbc6f0b3067804";
-    public final static String CONNEXION_API = "http://192.168.1.96:8383/CaveEscapeServer/API.php";
+    //public final static String CONNEXION_API = "http://192.168.1.96:8383/CaveEscapeServer/API.php";
     //public final static String CONNEXION_API = "http://192.168.0.14/CaveEscapeServer/API.php";
 
     public static String idClient;
