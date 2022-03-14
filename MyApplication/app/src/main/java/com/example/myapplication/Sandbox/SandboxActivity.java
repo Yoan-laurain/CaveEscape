28cfa1daf8cdcde14c00ffd085fd7a2d030c60dd
natoshiro40 <natoshirol@gmail.com>
2022-03-15 00:16:54 +0100

    setting new assets

--- MyApplication/app/src/main/java/com/example/myapplication/Sandbox/SandboxActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Sandbox/SandboxActivity.java
@@ -50,7 +50,7 @@ public class SandboxActivity extends AppCompatActivity
     Spinner spinnerLines;
     Spinner spinnerColumns;
     ArrayList<Integer> listNumbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8) ) ;
-    int[] images = {R.drawable.left_player,R.drawable.mur,R.drawable.blue_grass,R.drawable.opened_cage_blue,R.drawable.free_monster_blue,R.drawable.caged_monster_blue};
+    int[] images = {R.drawable.left_player_blue,R.drawable.mur,R.drawable.blue_grass,R.drawable.opened_cage_blue,R.drawable.free_monster_blue,R.drawable.caged_monster_blue};
     private int[] matrix;
     private int[] matrixTemp;
     private int count;
@@ -398,7 +398,7 @@ public class SandboxActivity extends AppCompatActivity
             {
                 switch ( matrix[ countNumber ] )
                 {
-                    case R.drawable.left_player :
+                    case R.drawable.left_player_blue:
                         content.append("P");
                         break;
 
