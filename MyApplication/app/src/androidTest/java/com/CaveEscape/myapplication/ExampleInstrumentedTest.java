c4c5aa845b50822fbed7ce788ebea38d7eb6af28
Yoan Laurain <80961060+Yoan-laurain@users.noreply.github.com>
2022-03-29 15:16:16 +0200

    rename com.example for store

--- MyApplication/app/src/androidTest/java/com/CaveEscape/myapplication/ExampleInstrumentedTest.java
+++ MyApplication/app/src/androidTest/java/com/CaveEscape/myapplication/ExampleInstrumentedTest.java
@@ -20,6 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.CaveEscape.myapplication", appContext.getPackageName());
     }
 }
